22b0a299127bc0a9f35e8916e6de80cbb6f96050
Gabriel Quiroz <gbrl_quiroz@hotmail.com>
2017-07-26 14:04:20 -0500

    se integrra el socket.io

--- app.js
+++ app.js
@@ -2,14 +2,31 @@ var app = require('express')();//app = express();
 var express = require('express');
 var http = require('http').createServer(app);
 var path = require('path');
-//corregir para evitar 'doble express'
+var io = require('socket.io')(http);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000,function(){
+io.on('connection', function (socket) {
+    console.log('usuario conectado');
+    
+    socket.on('join', function (myName) {
+        socket.name = myName;
+    });
+    socket.on('chat message', function (msg) {
+        //console.log('mensaje: ' + msg);
+        //io.emit('chat message', msg);
+        io.emit('chat message', socket.name + ': ' + msg);
+    });
+    socket.on('disconnect', function () {
+        console.log('usuario desconectado');
+    });
+});
+
+http.listen(3000, function () {
     console.log('escuchando en puerto 3000');
 });
 
